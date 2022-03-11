d73275a34138ea93783f09f64bcd6f87175e91ec
ac119820 <ac119820@8CG8163KZN>
2022-03-11 13:33:27 +1300

    Add project files.

---
+++ Exercise 6/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+double num1;
+Console.WriteLine("This pograme will tell you whether your number is positive, negative or zero.");
+num1 = Convert.ToDouble(Console.ReadLine());
+
+if (num1 < 0)
+{
+    Console.WriteLine("your number is negative");
+}
+if (num1 > 0)
+{
+    Console.WriteLine("your number is positive");
+}
+else (num1 = 0)
+{
+    Console.WriteLine("your number is zero");
+}
